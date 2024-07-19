6148286445bc1c40553ffebbc945599fed405e17
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-19 22:27:36 +0545

    test

--- hackerrannk_sets.py
+++ hackerrannk_sets.py
@@ -218,3 +218,55 @@
     
 #     # Print the result
 #     print(result)
+
+
+
+
+
+
+
+# # Task
+# # Apply your knowledge of the .add() operation to help your friend Rupal.
+# # Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection.
+# # She asked for your help. You pick the stamps one by one from a stack of N country stamps.
+# # Find the total number of distinct country stamps.
+
+# # Input Format
+# # The first line contains an integer N, the total number of country stamps.
+# # The next N lines contains the name of the country where the stamp is from.
+
+# # Constraints
+# # 0<N<1000
+
+# # Output Format
+# # Output the total number of distinct country stamps on a single line.
+
+# # Sample Input
+# # 7
+# # UK
+# # China
+# # USA
+# # France
+# # New Zealand
+# # UK
+# # France 
+
+# # Sample Output
+# # 5
+
+# # Explanation
+# # UK and France repeat twice. Hence, the total number of distinct country stamps is 5 (five).
+
+
+# def count_distinct_stamps(n, countries):
+#     country_set = set()
+#     for country in countries:
+#         country_set.add(country)
+#     return len(country_set)
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     countries = [input().strip() for _ in range(n)]
+#     result = count_distinct_stamps(n, countries)
+#     print(result)
+
