728c39d585ae26769f1ef90a8f94044a9cb8ab3e
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-19 22:17:09 +0545

    test

--- hackerrannk_sets.py
+++ hackerrannk_sets.py
@@ -108,3 +108,50 @@
 #     # Print each element in the sorted result on a new line
 #     for element in sorted_result:
 #         print(element)
+
+
+
+
+
+
+# # Task
+# # Now, let's use our knowledge of sets and help Mickey.
+# # Ms. Gabriel Williams is a botany professor at District College. One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
+
+# # Formula used:
+# # Function Description
+# # Complete the average function in the editor below.
+# # average has the following parameters:
+
+# # int arr: an array of integers
+# # Returns
+
+# # float: the resulting float value rounded to 3 places after the decimal
+# # Input Format
+
+# # The first line contains the integer, N, the size of arr.
+# # The second line contains the N space-separated integers,arr[i] .
+
+# # Constraints
+# # 0<N<=100
+
+# # Sample Input
+# # STDIN                                       Function
+# # -----                                       --------
+# # 10                                          arr[] size N = 10
+# # 161 182 161 154 176 170 167 171 170 174     arr = [161, 181, ..., 174]
+
+# # Sample Output
+# # 169.375
+
+
+
+# def average(array):
+#    distinct_heights = set(arr)  # Create a set to remove duplicates
+#    return round(sum(distinct_heights) / len(distinct_heights), 3)  # Calculate and return the average rounded to 3 decimal places
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = list(map(int, input().split()))
+#     result = average(arr)
+#     print(result)
