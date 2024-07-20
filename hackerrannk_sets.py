f14a6b24c8f7c64c93c9856c6a2c05e958716dd9
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-20 23:28:26 +0545

    test

--- hackerrannk_sets.py
+++ hackerrannk_sets.py
@@ -318,3 +318,65 @@
 #     countries = [input().strip() for _ in range(n)]
 #     result = count_distinct_stamps(n, countries)
 #     print(result)
+
+
+
+
+
+
+# # Task
+# # The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some have subscribed to only French and some have subscribed to both newspapers.
+# # You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at least one newspaper.
+
+# # Input Format
+# # The first line contains an integer,n , the number of students who have subscribed to the English newspaper.
+# # The second line contains n space separated roll numbers of those students.
+# # The third line contains b, the number of students who have subscribed to the French newspaper.
+# # The fourth line contains b space separated roll numbers of those students.
+
+# # Constraints
+# # 0<Total number of students in college < 1000
+
+# # Output Format
+# # Output the total number of students who have at least one subscription.
+
+# # Sample Input
+# # 9
+# # 1 2 3 4 5 6 7 8 9
+# # 9
+# # 10 1 2 3 11 21 55 6 8
+
+# # Sample Output
+# # 13
+
+
+
+
+
+
+# def count_unique_subscribers(n, english_roll_numbers, b, french_roll_numbers):
+#     # Convert roll numbers to sets
+#     english_set = set(english_roll_numbers)
+#     french_set = set(french_roll_numbers)
+    
+#     # Compute the union of both sets
+#     unique_subscribers = english_set.union(french_set)
+    
+#     # Return the number of unique subscribers
+#     return len(unique_subscribers)
+
+# if __name__ == '__main__':
+#     # Read the number of English subscribers
+#     n = int(input())
+#     # Read the roll numbers of English subscribers
+#     english_roll_numbers = list(map(int, input().split()))
+    
+#     # Read the number of French subscribers
+#     b = int(input())
+#     # Read the roll numbers of French subscribers
+#     french_roll_numbers = list(map(int, input().split()))
+    
+#     # Calculate the number of unique subscribers
+#     result = count_unique_subscribers(n, english_roll_numbers, b, french_roll_numbers)
+#     # Print the result
+#     print(result)
