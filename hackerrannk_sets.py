926ae40f3b756ca75c58603247eddeeb02042a8d
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-21 21:22:07 +0545

    test

--- hackerrannk_sets.py
+++ hackerrannk_sets.py
@@ -431,3 +431,57 @@
 
 # # Output the total number of students who have subscriptions to both newspapers
 # print(len(both_subscribers))
+
+
+
+
+
+
+
+# # Task
+# # Students of District College have a subscription to English and French newspapers. Some students have subscribed to only the English newspaper, some have subscribed to only the French newspaper, and some have subscribed to both newspapers.
+# # You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to only English newspapers.
+
+# # Input Format
+# # The first line contains the number of students who have subscribed to the English newspaper.
+# # The second line contains the space separated list of student roll numbers who have subscribed to the English newspaper.
+# # The third line contains the number of students who have subscribed to the French newspaper.
+# # The fourth line contains the space separated list of student roll numbers who have subscribed to the French newspaper.
+
+# # Constraints
+# # 0<Total number of students in college < 1000
+
+# # Output Format
+# # Output the total number of students who are subscribed to the English newspaper only.
+
+# # Sample Input
+# # 9
+# # 1 2 3 4 5 6 7 8 9
+# # 9
+# # 10 1 2 3 11 21 55 6 8
+
+# # Sample Output
+# # 4
+
+# # Explanation
+# # The roll numbers of students who only have English newspaper subscriptions are:4,5,7
+# #  and 9.
+# # Hence, the total is 4 students.
+
+
+
+# # Read the number of students subscribed to the English newspaper
+# n = int(input())
+# # Read the roll numbers of English subscribers
+# english_subscribers = set(map(int, input().split()))
+
+# # Read the number of students subscribed to the French newspaper
+# b = int(input())
+# # Read the roll numbers of French subscribers
+# french_subscribers = set(map(int, input().split()))
+
+# # Find the difference between the English and French subscribers sets
+# only_english_subscribers = english_subscribers - french_subscribers
+
+# # Output the total number of students who are subscribed to only the English newspaper
+# print(len(only_english_subscribers))
