4796adf80db537c7fd12e7cf9f829494837d443d
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-22 22:42:02 +0545

    test

--- hackerrannk_sets.py
+++ hackerrannk_sets.py
@@ -537,3 +537,71 @@
 
 # # Output the total number of students who have subscriptions to the English or the French newspaper but not both
 # print(len(exclusive_subscribers))
+
+
+
+
+# # TASK
+# # You are given a set A and N number of other sets. These N number of sets have to perform some specific mutation operations on set A.
+# # Your task is to execute those operations and print the sum of elements from set A.
+
+# # Input Format
+# # The first line contains the number of elements in set A.
+# # The second line contains the space separated list of elements in set A.
+# # The third line contains integer N, the number of other sets.
+# # The next 2*N lines are divided into N parts containing two lines each.
+# # The first line of each part contains the space separated entries of the operation name and the length of the other set.
+
+# # The second line of each part contains space separated list of elements in the other set A.
+# # 0<len(set(A))<1000
+# # 0<len(otherSets)<100
+# # 0<N<100
+
+# # Output Format
+# # Output the sum of elements in set A.
+
+# # Sample Input
+# #  16
+# #  1 2 3 4 5 6 7 8 9 10 11 12 13 14 24 52
+# #  4
+# #  intersection_update 10
+# #  2 3 5 6 8 9 1 4 7 11
+# #  update 2
+# #  55 66
+# #  symmetric_difference_update 5
+# #  22 7 35 62 58
+# #  difference_update 7
+# #  11 22 35 55 58 62 66
+
+# # Sample Output
+# # 38
+
+
+
+
+# # Step 1: Read inputs
+# num_elements = int(input())
+# A = set(map(int, input().split()))
+# N = int(input())
+
+# # Step 2: Process each operation
+# for _ in range(N):
+#     operation = input().split()[0]
+#     other_set = set(map(int, input().split()))
+    
+#     # Perform the appropriate operation on set A
+#     if operation == "intersection_update":
+#         A.intersection_update(other_set)
+#     elif operation == "update":
+#         A.update(other_set)
+#     elif operation == "symmetric_difference_update":
+#         A.symmetric_difference_update(other_set)
+#     elif operation == "difference_update":
+#         A.difference_update(other_set)
+
+# # Step 3: Output the sum of elements in set A
+# print(sum(A))
+
+
+
+
