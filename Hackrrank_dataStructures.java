b3459226fa653290b5485b64f57f661dd2b385b6
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-23 21:23:10 +0545

    test

--- Hackrrank_dataStructures.java
+++ Hackrrank_dataStructures.java
@@ -358,3 +358,109 @@
 //         scanner.close();
 //     }
 // }
+
+
+
+
+
+// // Let's play a game on an array! You're standing at index 0 of an n-element array named game. From some index i (where 0<=i<n), you can perform one of the following moves:
+// // Move Backward: If cell i-1 exists and contains a , you can walk back to cell i-1.
+// // Move Forward:
+// // If cell i+1 contains a zero, you can walk to cell i+1.
+// // If cell i+leap contains a zero, you can jump to cell i+leap.
+// // If you're standing in cell n-1 or the value of i+leap>=n, you can walk or jump off the end of the array and win the game.
+// // In other words, you can move from index i to index i+1, i-1, or i+leap as long as the destination index is a cell containing  a 0. If the destination index is greater than n-1, you win the game.
+
+// // Function Description
+// // Complete the canWin function in the editor below.
+
+// // canWin has the following parameters:
+
+// // int leap: the size of the leap
+// // int game[n]: the array to traverse
+
+// // Returns
+// // boolean: true if the game can be won, otherwise false
+
+// // Input Format
+// // The first line contains an integer,q , denoting the number of queries (i.e., function calls).
+// // The 2.q subsequent lines describe each query over two lines:
+
+// // The first line contains two space-separated integers describing the respective values of n and leap.
+// // The second line contains n space-separated binary integers (i.e., zeroes and ones) describing the respective values of game0, game1, ....., gamen-1.
+
+// // Constraints
+// // 1<=q<=5000
+// // 2<=n<=100
+// // 0<=leap<=100
+// // It is guaranteed that the value of game[0] is always 0.
+
+// // Sample Input
+// // STDIN           Function
+// // -----           --------
+// // 4               q = 4 (number of queries)
+// // 5 3             game[] size n = 5, leap = 3 (first query)
+// // 0 0 0 0 0       game = [0, 0, 0, 0, 0]
+// // 6 5             game[] size n = 6, leap = 5 (second query)
+// // 0 0 0 1 1 1     . . .
+// // 6 3
+// // 0 0 1 1 1 0
+// // 3 1
+// // 0 1 0
+
+// // Sample Output
+// // YES
+// // YES
+// // NO
+// // NO
+
+
+
+
+
+// import java.util.Scanner;
+
+// public class Solution {
+
+//     public static boolean canWin(int leap, int[] game) {
+//         return canWin(leap, game, 0);
+//     }
+
+//     private static boolean canWin(int leap, int[] game, int i) {
+//         // Base cases
+//         if (i >= game.length) {
+//             return true;  // If we've reached or passed the end of the array
+//         }
+//         if (i < 0 || game[i] == 1) {
+//             return false;  // If out of bounds or on a cell with a 1
+//         }
+
+//         // Mark this cell as visited
+//         game[i] = 1;
+
+//         // Explore the three possible moves
+//         boolean moveForward = canWin(leap, game, i + 1);
+//         boolean jump = canWin(leap, game, i + leap);
+//         boolean moveBackward = canWin(leap, game, i - 1);
+
+//         return moveForward || jump || moveBackward;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int q = scanner.nextInt();
+
+//         for (int i = 0; i < q; i++) {
+//             int n = scanner.nextInt();
+//             int leap = scanner.nextInt();
+
+//             int[] game = new int[n];
+//             for (int j = 0; j < n; j++) {
+//                 game[j] = scanner.nextInt();
+//             }
+
+//             System.out.println((canWin(leap, game)) ? "YES" : "NO");
+//         }
+//         scanner.close();
+//     }
+// }
