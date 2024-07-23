34ccac7395eff7db15a72ec3c84771d9142b685e
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-23 21:29:45 +0545

    test

--- Hackrrank_dataStructures.java
+++ Hackrrank_dataStructures.java
@@ -464,3 +464,81 @@
 //         scanner.close();
 //     }
 // }
+
+
+
+
+
+
+
+// // You are given a phone book that consists of people's names and their phone number. After that you will be given some person's name as query. For each query, print the phone number of that person.
+
+// // Input Format
+// // The first line will have an integer n denoting the number of entries in the phone book. Each entry consists of two lines: a name and the corresponding phone number.
+// // After these, there will be some queries. Each query will contain a person's name. Read the queries until end-of-file.
+
+// // Constraints:
+// // A person's name consists of only lower-case English letters and it may be in the format 'first-name last-name' or in the format 'first-name'. Each phone number has exactly 8 digits without any leading zeros.
+// // 1<=n<=100000
+// // 1<=Query<=100000
+
+// // Output Format
+// // For each case, print "Not found" if the person has no entry in the phone book. Otherwise, print the person's name and phone number. See sample output for the exact format.
+// // To make the problem easier, we provided a portion of the code in the editor. You can either complete that code or write completely on your own.
+
+// // Sample Input
+// // 3
+// // uncle sam
+// // 99912222
+// // tom
+// // 11122222
+// // harry
+// // 12299933
+// // uncle sam
+// // uncle tom
+// // harry
+
+// // Sample Output
+// // uncle sam=99912222
+// // Not found
+// // harry=12299933
+
+
+
+
+// import java.util.*;
+// import java.io.*;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         Map<String, String> phoneBook = new HashMap<>();
+        
+//         // Read number of entries in the phone book
+//         int n = scanner.nextInt();
+//         scanner.nextLine(); // Consume the remaining newline character
+        
+//         // Read the entries and store them in the phone book
+//         for (int i = 0; i < n; i++) {
+//             String name = scanner.nextLine();
+//             String phoneNumber = scanner.nextLine();
+//             phoneBook.put(name, phoneNumber);
+//         }
+        
+//         // Read queries until end-of-file and output results
+//         while (scanner.hasNextLine()) {
+//             String query = scanner.nextLine();
+//             if (phoneBook.containsKey(query)) {
+//                 System.out.println(query + "=" + phoneBook.get(query));
+//             } else {
+//                 System.out.println("Not found");
+//             }
+//         }
+        
+//         scanner.close();
+//     }
+// }
+
+
+
+
