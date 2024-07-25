1db33d8c9cca4823e7d287ebbf395b1cd8bc6078
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-25 23:31:03 +0545

    test

--- hackerrank_DateAndTime.py
+++ hackerrank_DateAndTime.py
@@ -104,3 +104,21 @@
 # if __name__ == "__main__":
 #     main()
 
+
+
+
+
+
+# Input reading
+A = set(input().split())
+n = int(input())
+
+is_strict_superset = True
+
+for _ in range(n):
+    other_set = set(input().split())
+    if not (A > other_set):  # Check if A is a strict superset of other_set
+        is_strict_superset = False
+        break
+
+print(is_strict_superset)
