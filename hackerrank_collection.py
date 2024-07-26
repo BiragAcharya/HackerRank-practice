b0a93ecc2cd36ae2f3dec178cf47e3c5d70d9de4
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-26 22:27:37 +0545

    test

--- hackerrank_collection.py
+++ hackerrank_collection.py
@@ -60,3 +60,55 @@
 # columns = input().split()
 # marks_idx = columns.index("MARKS")
 # print(f"{sum(int(input().split()[marks_idx]) for _ in range(N)) / N:.2f}")
+
+
+
+
+
+
+
+# Task
+# You are the manager of a supermarket.
+# You have a list of N items together with their prices that consumers bought on a particular day.
+# Your task is to print each item_name and net_price in order of its first occurrence.
+
+# item_name = Name of the item.
+# net_price = Quantity of the item sold multiplied by the price of each item.
+
+# Input Format
+# The first line contains the number of items,N .
+# The next N lines contains the item's name and price, separated by a space.
+
+# Constraints
+# 0<N<=100
+
+# Output Format
+# Print the item_name and net_price in order of its first occurrence.
+
+# Sample Input
+# 9
+# BANANA FRIES 12
+# POTATO CHIPS 30
+# APPLE JUICE 10
+# CANDY 5
+# APPLE JUICE 10
+# CANDY 5
+# CANDY 5
+# CANDY 5
+# POTATO CHIPS 30
+
+# Sample Output
+# BANANA FRIES 12
+# POTATO CHIPS 60
+# APPLE JUICE 20
+# CANDY 20
+
+# Explanation
+# BANANA FRIES: Quantity bought: , Price: 12
+# Net Price: 12
+# POTATO CHIPS: Quantity bought: , Price: 30
+# Net Price: 60
+# APPLE JUICE: Quantity bought: , Price: 10
+# Net Price: 20
+# CANDY: Quantity bought: , Price: 5
+# Net Price: 20
