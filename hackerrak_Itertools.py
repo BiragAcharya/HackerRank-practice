65bc281f1f825c662ce10f7d7edd809bc26cf136
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-27 22:53:01 +0545

    test

--- hackerrak_Itertools.py
+++ hackerrak_Itertools.py
@@ -1,44 +1,44 @@
-Task
-You are given a two lists A and B. Your task is to compute their cartesian product A X B.
+# # Task
+# # You are given a two lists A and B. Your task is to compute their cartesian product A X B.
 
-Example
-A = [1, 2]
-B = [3, 4]
+# # Example
+# # A = [1, 2]
+# # B = [3, 4]
 
-AxB = [(1, 3), (1, 4), (2, 3), (2, 4)]
-Note: A and B are sorted lists, and the cartesian product's tuples should be output in sorted order.
+# # AxB = [(1, 3), (1, 4), (2, 3), (2, 4)]
+# # Note: A and B are sorted lists, and the cartesian product's tuples should be output in sorted order.
 
-Input Format
-The first line contains the space separated elements of list A.
-The second line contains the space separated elements of list B.
+# # Input Format
+# # The first line contains the space separated elements of list A.
+# # The second line contains the space separated elements of list B.
 
-Both lists have no duplicate integer elements.
+# # Both lists have no duplicate integer elements.
 
-Constraints
-0<A<30
-0<B<30
+# # Constraints
+# # 0<A<30
+# # 0<B<30
 
-Output Format
-Output the space separated tuples of the cartesian product.
+# # Output Format
+# # Output the space separated tuples of the cartesian product.
 
-Sample Input
- 1 2
- 3 4
+# # Sample Input
+# #  1 2
+# #  3 4
 
-Sample Output
- (1, 3) (1, 4) (2, 3) (2, 4)
-
-
+# # Sample Output
+# #  (1, 3) (1, 4) (2, 3) (2, 4)
 
 
-from itertools import product
 
-# Reading the input lists
-A = list(map(int, input().strip().split()))
-B = list(map(int, input().strip().split()))
 
-# Calculating the Cartesian product
-cartesian_product = list(product(A, B))
+# from itertools import product
 
-# Printing the output in the required format
-print(' '.join(map(str, cartesian_product)))
+# # Reading the input lists
+# A = list(map(int, input().strip().split()))
+# B = list(map(int, input().strip().split()))
+
+# # Calculating the Cartesian product
+# cartesian_product = list(product(A, B))
+
+# # Printing the output in the required format
+# print(' '.join(map(str, cartesian_product)))
