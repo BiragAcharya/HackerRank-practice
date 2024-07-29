f7d47d933c63930ac350305654a4b6cdb548202d
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-29 22:17:00 +0545

    test

--- math.py
+++ math.py
@@ -39,43 +39,43 @@
 
 
 
-You are given a positive integer N. Print a numerical triangle of height N-1 like the one below:
 
-1
-22
-333
-4444
-55555
-......
-Can you do it using only arithmetic operations, a single for loop and print statement?
+# # You are given a positive integer N. Print a numerical triangle of height N-1 like the one below:
 
-Use no more than two lines. The first line (the for statement) is already written for you. You have to complete the print statement.
+# # 1
+# # 22
+# # 333
+# # 4444
+# # 55555
+# # ......
+# # Can you do it using only arithmetic operations, a single for loop and print statement?
 
-Note: Using anything related to strings will give a score of 0.
+# # Use no more than two lines. The first line (the for statement) is already written for you. You have to complete the print statement.
 
-Input Format
-A single line containing integer, N.
+# # Note: Using anything related to strings will give a score of 0.
 
-Constraints
-1<=N<=9
+# # Input Format
+# # A single line containing integer, N.
 
-Output Format
-Print N-1 lines as explained above.
+# # Constraints
+# # 1<=N<=9
 
-Sample Input
-5
+# # Output Format
+# # Print N-1 lines as explained above.
 
-Sample Output
-1
-22
-333
-4444
+# # Sample Input
+# # 5
 
-
-
+# # Sample Output
+# # 1
+# # 22
+# # 333
+# # 4444
 
 
 
-for i in range(1, int(input())): print(i * (10**i - 1) // 9)
+
+
+# for i in range(1, int(input())): print(i * (10**i - 1) // 9)
 
 
