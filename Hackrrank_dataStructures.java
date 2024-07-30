c917f51d5ff64faed3682667267bb300c0a58929
BiragAcharya <Biragacharya2062@gmail.com>
2024-07-30 21:05:55 +0545

    test

--- Hackrrank_dataStructures.java
+++ Hackrrank_dataStructures.java
@@ -615,102 +615,102 @@
 
 
 
-You are given a list of student information: ID, FirstName, and CGPA. Your task is to rearrange them according to their CGPA in decreasing order.
-If two student have the same CGPA, then arrange them according to their first name in alphabetical order. If those two students also have the same first name, then order them according to their ID. No two students have the same ID.
-Hint: You can use comparators to sort a list of objects. See the oracle docs to learn about comparators.
+// // You are given a list of student information: ID, FirstName, and CGPA. Your task is to rearrange them according to their CGPA in decreasing order.
+// // If two student have the same CGPA, then arrange them according to their first name in alphabetical order. If those two students also have the same first name, then order them according to their ID. No two students have the same ID.
+// // Hint: You can use comparators to sort a list of objects. See the oracle docs to learn about comparators.
 
-Input Format
-The first line of input contains an integer N, representing the total number of students. The next N lines contains a list of student information in the following structure:
-ID Name CGPA
+// // Input Format
+// // The first line of input contains an integer N, representing the total number of students. The next N lines contains a list of student information in the following structure:
+// // ID Name CGPA
 
-Constraints
-2<=N<=1000
-0<=ID<=100000
-5<=|Name|<=30
-0<=CGPA<=4.00
-
-
-The name contains only lowercase English letters. The  IDcontains only integer numbers without leading zeros. The CGPA will contain, at most, 2 digits after the decimal point.
-
-Output Format
-After rearranging the students according to the above rules, print the first name of each student on a separate line.
-
-Sample Input
-5
-33 Rumpa 3.68
-85 Ashis 3.85
-56 Samiha 3.75
-19 Samara 3.75
-22 Fahim 3.76
-
-Sample Output
-Ashis
-Fahim
-Samara
-Samiha
-Rumpa
+// // Constraints
+// // 2<=N<=1000
+// // 0<=ID<=100000
+// // 5<=|Name|<=30
+// // 0<=CGPA<=4.00
 
 
+// // The name contains only lowercase English letters. The  IDcontains only integer numbers without leading zeros. The CGPA will contain, at most, 2 digits after the decimal point.
+
+// // Output Format
+// // After rearranging the students according to the above rules, print the first name of each student on a separate line.
+
+// // Sample Input
+// // 5
+// // 33 Rumpa 3.68
+// // 85 Ashis 3.85
+// // 56 Samiha 3.75
+// // 19 Samara 3.75
+// // 22 Fahim 3.76
+
+// // Sample Output
+// // Ashis
+// // Fahim
+// // Samara
+// // Samiha
+// // Rumpa
 
 
-import java.util.*;
 
-class Student {
-    private int id;
-    private String fname;
-    private double cgpa;
 
-    public Student(int id, String fname, double cgpa) {
-        this.id = id;
-        this.fname = fname;
-        this.cgpa = cgpa;
-    }
+// import java.util.*;
 
-    public int getId() {
-        return id;
-    }
+// class Student {
+//     private int id;
+//     private String fname;
+//     private double cgpa;
 
-    public String getFname() {
-        return fname;
-    }
+//     public Student(int id, String fname, double cgpa) {
+//         this.id = id;
+//         this.fname = fname;
+//         this.cgpa = cgpa;
+//     }
 
-    public double getCgpa() {
-        return cgpa;
-    }
-}
+//     public int getId() {
+//         return id;
+//     }
 
-public class Solution {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
+//     public String getFname() {
+//         return fname;
+//     }
 
-        List<Student> studentList = new ArrayList<Student>();
-        while (testCases > 0) {
-            int id = in.nextInt();
-            String fname = in.next();
-            double cgpa = in.nextDouble();
+//     public double getCgpa() {
+//         return cgpa;
+//     }
+// }
 
-            Student st = new Student(id, fname, cgpa);
-            studentList.add(st);
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner in = new Scanner(System.in);
+//         int testCases = Integer.parseInt(in.nextLine());
 
-            testCases--;
-        }
+//         List<Student> studentList = new ArrayList<Student>();
+//         while (testCases > 0) {
+//             int id = in.nextInt();
+//             String fname = in.next();
+//             double cgpa = in.nextDouble();
 
-        Collections.sort(studentList, new Comparator<Student>() {
-            @Override
-            public int compare(Student s1, Student s2) {
-                if (s1.getCgpa() != s2.getCgpa()) {
-                    return Double.compare(s2.getCgpa(), s1.getCgpa()); // descending order of CGPA
-                } else if (!s1.getFname().equals(s2.getFname())) {
-                    return s1.getFname().compareTo(s2.getFname()); // ascending order of names
-                } else {
-                    return Integer.compare(s1.getId(), s2.getId()); // ascending order of IDs
-                }
-            }
-        });
+//             Student st = new Student(id, fname, cgpa);
+//             studentList.add(st);
 
-        for (Student st : studentList) {
-            System.out.println(st.getFname());
-        }
-    }
-}
+//             testCases--;
+//         }
+
+//         Collections.sort(studentList, new Comparator<Student>() {
+//             @Override
+//             public int compare(Student s1, Student s2) {
+//                 if (s1.getCgpa() != s2.getCgpa()) {
+//                     return Double.compare(s2.getCgpa(), s1.getCgpa()); // descending order of CGPA
+//                 } else if (!s1.getFname().equals(s2.getFname())) {
+//                     return s1.getFname().compareTo(s2.getFname()); // ascending order of names
+//                 } else {
+//                     return Integer.compare(s1.getId(), s2.getId()); // ascending order of IDs
+//                 }
+//             }
+//         });
+
+//         for (Student st : studentList) {
+//             System.out.println(st.getFname());
+//         }
+//     }
+// }
