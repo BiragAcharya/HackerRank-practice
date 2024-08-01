675589dd8b1baff4a0dba29dd2eb60c7b1eac087
BiragAcharya <Biragacharya2062@gmail.com>
2024-08-01 19:33:17 +0545

    test

--- Hackrrank_dataStructures.java
+++ Hackrrank_dataStructures.java
@@ -987,3 +987,48 @@ amy 100
 david 100
 aakansha 75
 heraldo 50
+
+
+
+import java.util.*;
+
+// Checker class implementing Comparator<Player>
+class Checker implements Comparator<Player> {
+    public int compare(Player a, Player b) {
+        if (a.score != b.score) {
+            return b.score - a.score; // descending order by score
+        } else {
+            return a.name.compareTo(b.name); // alphabetical order by name
+        }
+    }
+}
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+     
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
