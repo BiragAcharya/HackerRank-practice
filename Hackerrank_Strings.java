3540196278e7bb0be57be8a192f287ba9d9d66ca
BiragAcharya <Biragacharya2062@gmail.com>
2024-08-02 19:31:13 +0545

    test

--- Hackerrank_Strings.java
+++ Hackerrank_Strings.java
@@ -183,3 +183,38 @@
 //         in.close();
 //     }
 // }
+
+
+
+
+
+
+
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+
+class MyRegex {
+    String pattern;
+
+    public MyRegex() {
+        // Regular expression for a valid IP address
+        String zeroTo255 = "((25[0-5])|(2[0-4][0-9])|([01]?[0-9][0-9]?)|0)";
+        this.pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+    }
+}
