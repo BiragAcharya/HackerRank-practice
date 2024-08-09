d6e85454bc33d0d77b40c3538b4df272ee5b3c5b
BiragAcharya <Biragacharya2062@gmail.com>
2024-08-09 20:11:46 +0545

    test

--- hackerrank_collection.py
+++ hackerrank_collection.py
@@ -237,3 +237,25 @@
 
 # # Output the total money earned
 # print(total_earned)
+
+
+
+
+
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's 0 key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+Write a query calculating the amount of error (i.e.: actual-miscalculated  average monthly salaries), and round it up to the next integer.
+
+Input Format
+The EMPLOYEES table is described as follows:
+
+
+Note: Salary is per month.
+
+Constraints
+1000<Salary<10^5.
+
+Sample Input
+
+
+Sample Output
+2061
