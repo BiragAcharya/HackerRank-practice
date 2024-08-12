cc26b9823b7a34175fd875c99fc6d9a8ecae2076
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-08-12 20:42:26 +0545

    Update Java_oop.java

--- Java_oop.java
+++ Java_oop.java
@@ -76,6 +76,7 @@
 
 
 
+
 // // Write the following code in your editor below:
 
 // // 1. A class named Arithmetic with a method named add that takes 2 integers as parameters and returns an integer denoting their sum.
