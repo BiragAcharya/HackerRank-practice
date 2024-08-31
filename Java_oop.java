c0c9c2fee7fa494ae996d9e5febf3e4eebdcd8d0
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-08-31 18:44:39 +0545

    Update Java_oop.java

--- Java_oop.java
+++ Java_oop.java
@@ -77,7 +77,6 @@
 
 
 // // Write the following code in your editor below:
-
 // // 1. A class named Arithmetic with a method named add that takes 2 integers as parameters and returns an integer denoting their sum.
 // // 2. A class named Adder that inherits from a superclass named Arithmetic.
 // // Your classes should not be be public.
