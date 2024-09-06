094c942866c35531c36ca9d725b425b479df8b44
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-09-06 21:06:34 +0545

    Update Hackerrank_exceptionhandling.java

--- Hackerrank_exceptionhandling.java
+++ Hackerrank_exceptionhandling.java
@@ -1,5 +1,3 @@
-
-
 // // Exception handling is the process of responding to the occurrence, during computation, of exceptions – anomalous or exceptional conditions requiring special processing – often changing the normal flow of program execution. (Wikipedia)
 // // Java has built-in mechanism to handle exceptions. Using the try statement we can test a block of code for errors. The catch block contains the code that says what to do if exception occurs.
 // // This problem will test your knowledge on try-catch block.
@@ -51,7 +49,6 @@
 //         scanner.close();
 //     }
 // }
-
 
 
 
