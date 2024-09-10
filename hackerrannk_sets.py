0e2347b18a0bab1a9806a5ddc7205714f5465131
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-09-10 19:25:04 +0545

    Update hackerrannk_sets.py

--- hackerrannk_sets.py
+++ hackerrannk_sets.py
@@ -1,6 +1,5 @@
 # # A set is an unordered collection of elements without duplicate entries.
 # # When printed, iterated or converted into a sequence, its elements will appear in an arbitrary order.
-
 # # Example
 # # >>> print set()
 # # set([])
