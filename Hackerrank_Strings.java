7a2ce34da6faac0101b280b76f504eda66cec10f
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-09-19 21:22:05 +0545

    Update Hackerrank_Strings.java

--- Hackerrank_Strings.java
+++ Hackerrank_Strings.java
@@ -6,7 +6,6 @@
 // // The elements of a String are called characters. The number of characters in a String is called the length, and it can be retrieved with the String.length() method.
 
 // // Given two strings of lowercase English letters,  A and B, perform the following operations:
-
 // // Sum the lengths of A and B.
 // // Determine if A is lexicographically larger than B (i.e.: does B  come before A in the dictionary?).
 // // Capitalize the first letter in A and B and print them on a single line, separated by a space.
