0a6f6c5e293c1851c628e20f2fd5a4fc3e7d2176
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-09-21 21:50:01 +0545

    Update hackerrank_introduction.py

--- hackerrank_introduction.py
+++ hackerrank_introduction.py
@@ -4,7 +4,7 @@
 # # If  is odd, print Weird
 # # If  is even and in the inclusive range of  to , print Not Weird
 # # If  is even and in the inclusive range of  to , print Weird
-# # If  is even and greater than , print Not Weird
+# # If  is even and greater than, print Not Weird
 
 
 # n = int(input("Enter number: ").strip())
