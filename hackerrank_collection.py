c878602488d9c035c84b53b362eff202a29f26db
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-09-21 21:51:26 +0545

    Update hackerrank_collection.py

--- hackerrank_collection.py
+++ hackerrank_collection.py
@@ -2,7 +2,7 @@
 # # Dr. John Wesley has a spreadsheet containing a list of student's IDs, marks ,class  and name.
 # # Your task is to help Dr. Wesley calculate the average marks of the students.
 
-# # Average = Sum of all marks/TOtal students
+# # Average = Sum of all marks/Total students
 
 # # Note:
 # # 1. Columns can be in any order. IDs, marks, class and name can be written in any order in the spreadsheet.
