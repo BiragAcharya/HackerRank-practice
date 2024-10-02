1712999e29d53145144a654657aeeaddf7b1996a
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-10-02 22:36:21 +0545

    Update Hackrrank_dataStructures.java

--- Hackrrank_dataStructures.java
+++ Hackrrank_dataStructures.java
@@ -27,7 +27,7 @@
 // // Good luck!
 
 // // Input Format
-// // The first line contains a single integer, , denoting the size of the array.
+// // The first line contains a single integer,  denoting the size of the array.
 // // Each line i of the n subsequent lines contains a single integer denoting the value of element ai.
 
 // // Output Format
