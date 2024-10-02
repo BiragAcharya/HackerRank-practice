338a21a1ad2b5ae10ab3ceda9f225d6ee7a0e233
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-10-02 22:35:01 +0545

    Update Hackrrank_dataStructures.java

--- Hackrrank_dataStructures.java
+++ Hackrrank_dataStructures.java
@@ -1,7 +1,7 @@
 // public class Hackrrank_dataStructures {
 // }
 
-// // To create an array of integers named  that can hold four integer values, you would write the following code:
+// // To create an array of integers named  that can hold four integer values,you would write the following code:
 
 // // int[] myArray = new int[4];
 // // This sets aside a block of memory that's capable of storing  integers. Each integer storage cell is assigned a unique index ranging from  to one less than the size of the array, and each cell initially contains a . In the case of , we can store integers at indices , , , and . Let's say we wanted the last cell to store the number ; to do this, we write:
