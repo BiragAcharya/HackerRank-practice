b1d9de5e066616e8c189bf9fc1a49fe2834f89e4
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-10-02 22:34:30 +0545

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -72,7 +72,6 @@
 
 // // Task
 // // Given an integer, , perform the following conditional actions:
-
 // // If  is odd, print Weird
 // // If  is even and in the inclusive range of  to , print Not Weird
 // // If  is even and in the inclusive range of  to , print Weird
