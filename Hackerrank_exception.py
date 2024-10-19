56fb1b293b45d846a5dca893b39bd876aaf0c208
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-10-19 21:17:23 +0545

    Update Hackerrank_exception.py

--- Hackerrank_exception.py
+++ Hackerrank_exception.py
@@ -17,7 +17,6 @@
 # # >>> ValueError: invalid literal for int() with base 10: '#'
 # # To learn more about different built-in exceptions click here.
 
-
 # # Handling Exceptions
 # # The statements try and except can be used to handle selected exceptions. A try statement may have more than one except clause to specify handlers for different exceptions.
 
