41667864565f76ab991f0eb66938a8f8b26e710f
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-10-23 21:44:44 +0545

    Update Hackerrank_exceptionhandling.java

--- Hackerrank_exceptionhandling.java
+++ Hackerrank_exceptionhandling.java
@@ -59,7 +59,6 @@
 
 
 
-
 // // You are required to compute the power of a number by implementing a calculator.
 // // Create a class MyCalculator which consists of a single method long power(int, int). This method takes two integers, n and p, as parameters and finds n^p.
 // // If either n or p is negative, then the method must throw an exception which says "n or p should not be negative". Also, if both n and p are zero, then the method must throw an exception which says "n and p should not be zero"
