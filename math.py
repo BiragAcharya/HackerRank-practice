1685bd6aeb16623d42df4f3eae67ec7ae38cbbc0
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-11-04 20:51:51 +0545

    Update math.py

--- math.py
+++ math.py
@@ -1,7 +1,6 @@
 # # Task
 
 # # You are given a complex z. Your task is to convert it to polar coordinates.
-
 # # Input Format
 # # A single line containing the complex number z. Note: complex() function can be used in python to convert the input as a complex number.
 
