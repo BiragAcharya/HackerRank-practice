27a3f78d3a2eafb7240b503e6ce1f911a2df9979
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-11-08 21:18:11 +0545

    Update hackerrank_DateAndTime.py

--- hackerrank_DateAndTime.py
+++ hackerrank_DateAndTime.py
@@ -1,6 +1,5 @@
 # # Task
 # # You are given a date.Your task is to find what the day is on that date.
-
 # # Input Format
 # # A single line of input containing the space separated month, day and year, respectively, in  MM|DD|YYYY  format.
 
