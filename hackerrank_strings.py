61f44758bc1596942c1dd6289f9d1d8a13df5900
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-11-17 21:00:37 +0545

    Update hackerrank_strings.py

--- hackerrank_strings.py
+++ hackerrank_strings.py
@@ -68,7 +68,6 @@
 
 
 
-
 # # You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
 # # Hello firstname lastname! You just delved into python.
 
