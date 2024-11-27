df0845ef2824c398dc83cb8317b04b217be9d274
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-11-27 20:56:55 +0545

    Update Java_oop.java

--- Java_oop.java
+++ Java_oop.java
@@ -120,7 +120,6 @@
 
 
 
-
 // // A Java abstract class is a class that can't be instantiated. That means you cannot create new instances of an abstract class.
 // //  It works as a base for subclasses. You should learn about Java Inheritance before attempting this challenge.
 
