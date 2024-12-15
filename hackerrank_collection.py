d7a127af05ba7a8120d69a324df8020d991c41d0
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2024-12-15 22:29:51 +0545

    Update hackerrank_collection.py

--- hackerrank_collection.py
+++ hackerrank_collection.py
@@ -122,7 +122,6 @@
 #     print(f"{item_name} {net_price}")
 
 
-
 # collections.Counter()
 # A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
 
