9869ec8e250428a01e6242c4d660e2fce38101bd
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2025-01-12 22:53:42 +0545

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -85,7 +85,6 @@
 // public class Solution {
 //     public static void main(String[] args) {
 //         Scanner scanner = new Scanner(System.in);
-        
 //         int n = scanner.nextInt();
 //         scanner.close();
         
