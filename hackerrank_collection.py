29c6dfdf8c3c0975cb565dd3c1ec26f1f1cce30e
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2025-01-17 18:49:22 +0545

    Update hackerrank_collection.py

--- hackerrank_collection.py
+++ hackerrank_collection.py
@@ -6,7 +6,7 @@
 
 # # Note:
 # # 1. Columns can be in any order. IDs, marks, class and name can be written in any order in the spreadsheet.
-# # 2. Column names are ID, MARKS, CLASS and NAME. (The spelling and case type of these names won't change.)
+# # 2. Column names are ID, MARKS, CLASS and NAME. (The spelling and case type of these names won't change)
 
 # # Input Format
 # # The first line contains an integer N, the total number of students.
