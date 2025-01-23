3e4f9693742f46bf5a3dd2bde2f44fb00792b8c9
BiragAcharya <146616808+BiragAcharya@users.noreply.github.com>
2025-01-23 20:12:34 +0545

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -146,7 +146,7 @@
 
 
 
-// // Java's System.out.printf function can be used to print formatted output. The purpose of this exercise is to test your understanding of formatting output using printf.
+// // Java's System.out.printf function can be used to print formatted output. The purpose of this exercise is to test your understanding of formatting output using print f.
 // // To get you started, a portion of the solution is provided for you in the editor; you must format and print the input to complete the solution.
 
 // // Input Format
